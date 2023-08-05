d022a037792f31f272ed646f889dca68cd8a44fd
AboodXD <aboood40091@gmail.com>
2023-08-05 17:13:23 +0400

    Remove headers to prepare for headers repo

--- game/include/actor/MapActor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <basis/seadTypes.h>
-
-class MapActor
-{
-public:
-    static const u32 cProfileID[];
-};
--- game/include/effect/FollowEffect.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <effect/Effect.h>
-
-class FollowEffect : public Effect
-{
-    // NSMBW: dEf::followEffect_c
-};
-static_assert(sizeof(FollowEffect) == sizeof(Effect));
